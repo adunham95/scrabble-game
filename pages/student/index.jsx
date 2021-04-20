dc18c392a3232841f493f90f4c2173e6b3903ec0
Adrian Dunham <adunham95@gmail.com>
2021-04-20 16:38:43 -0400

    Styled login screen

--- pages/student/index.jsx
+++ pages/student/index.jsx
@@ -1,49 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Icon } from '../../components/Icons/Icon';
 
-const avatars = ['cat','dog','dragon','elephant','horse','squirrel','turtle','unicorn','whale'];
+const avatars = ['cat', 'dog', 'dragon', 'elephant', 'horse', 'squirrel', 'turtle', 'unicorn', 'whale'];
 const colors = [
-    {name: "Red", color: '#f44336'},
-    {name: "Pink", color: '#e91e63'},
-    {name: "Purple", color: '#673ab7'},
-    {name: "Blue", color: '#2196f3'},
-    {name: "Green", color: '#009688'},
-    {name: "Lime", color: '#cddc39'},
-    {name: "Yellow", color: '#ffeb3b'},
-    {name: "Amber", color: '#ffc107'},
-    {name: "Orange", color: '#FF8201'},
-    {name: "Smoke", color: '#58595C'}
-]
+  { name: 'Red', color: '#f44336' },
+  { name: 'Pink', color: '#e91e63' },
+  { name: 'Purple', color: '#673ab7' },
+  { name: 'Blue', color: '#2196f3' },
+  { name: 'Green', color: '#009688' },
+  { name: 'Lime', color: '#cddc39' },
+  { name: 'Yellow', color: '#f1dd2f' },
+  { name: 'Amber', color: '#ffc107' },
+  { name: 'Orange', color: '#FF8201' },
+  { name: 'Smoke', color: '#58595C' },
+];
 
 const Index = () => {
+  const [selectedAvatar, setSelectedAvatar] = useState('dog');
+  const [selectedColor, setSelectedColor] = useState({ name: 'Smoke', color: '#58595C' });
 
-    const [selectedAvatar, setSelectedAvatar] = useState("dog")
-    const [selectedColor, setSelectedColor] = useState({name: "Smoke", color: "#58595C"})
-
-    return (
-        <div>
-            <div>
+  return (
+    <div className="loginScreen">
+      <div>
+        <h1>Join Game</h1>
+      </div>
+      <div className="main-icon">
+        <Icon
+          name={selectedAvatar}
+          color={selectedColor.color}
+          height={100}
+          width={100}
+        />
+      </div>
+      <div className="list-icons">
+        {
+            avatars.map((a) => (
+              <button onClick={() => setSelectedAvatar(a)}>
                 <Icon
-                    name={selectedAvatar}
-                    color={selectedColor.color}
-                    height={100}
-                    width={100}
+                  name={a}
+                  height={35}
+                  width={35}
                 />
-            </div>
-            <div>
-                {
-                    avatars.map(a => <button onClick={()=>setSelectedAvatar(a)}>
-                        <Icon name={a}/>
-                    </button>)
-                }
-            </div>
-            <div>
-                {
-                    colors.map(c => <button onClick={()=>setSelectedColor(c)} style={{backgroundColor:c.color}}>{c.name}</button>)
-                }
-            </div>
-        </div>
-    )
-}
+              </button>
+            ))
+        }
+      </div>
+      <div className="list-colors">
+        {
+            colors.map((c) => (
+              <button
+                onClick={() => setSelectedColor(c)}
+                style={{ backgroundColor: c.color }}
+              >
+                {c.name}
+              </button>
+            ))
+        }
+      </div>
+      <div className="login">
+        <input placeholder="Enter Password" />
+      </div>
+      <div className="login">
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
 
-export default Index
+export default Index;
